b7076643a794aa14e2e439b5a51ec4e8e4492a56
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-08-24 15:29:56 +0200

    Reset projet
    No more react

--- sammanslaget/src/gameController.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GameController : MonoBehaviour {
-  private void Start () {
-#if (UNITY_WEBGL == true && UNITY_EDITOR == false)
-    WebGLInput.captureAllKeyboardInput = false;
-#endif
-  }
-}
