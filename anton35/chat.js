25927608e026d074236d13a0a1567da7d74fa695
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-08-27 10:44:22 +0200

    Moved clicks to stories
    moved from animations to stories

--- anton35/chat.js
+++ anton35/chat.js
@@ -15,6 +15,53 @@ const pcMsg = [
 
 ]
 
+msgBar.addEventListener('click', function() {
+    
+    let chatHeight = chatHistory.offsetHeight;
+    let msgBarStyle = window.getComputedStyle(msgBar);
+    let msgBarPos = msgBarStyle.transform;
+   
+    
+    
+    
+  //   console.log(msgBarPos); // (1, 0, 0, 1, 0, 350)
+  //   console.log(chatHeight);
+  if(PcBubleCounter < 4){
+      
+  
+    if(chatHeight === 450){
+        chatHistory.style.height = "175px";
+        msgBar.style.transform = "translateY(210px)";
+      //   console.log(chatHistory.offsetHeight);
+        updateScroll(chatHistory);
+  
+    }
+    else if(chatHeight == 175){
+        chatHistory.style.height = "450px";
+      //   console.log({chatHeight});
+        msgBar.style.transform = "translateY(453px)";
+        updateScroll(chatHistory);
+  
+    }
+    else{
+      console.log('unkown offset: '+chatHeight);
+    }
+  }
+  else{
+      const donation = document.querySelector(".donation-buble");
+      donation.style.transform = 'translateX(-50%) translateY(780px)';
+  }
+  })
+  
+  function updateScroll(elmentPan){
+      setTimeout(()=>{
+  
+          elmentPan.scrollTop = elmentPan.scrollHeight;
+      }, 600);
+  }
+  
+
+
 const npcBuble = document.createElement("P");
 npcBuble.classList.add("npc-bubble");
 
